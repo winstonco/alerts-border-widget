8c59dfe69a3fd33ec814757407ac4b852884c06a
Winston Co <winstonco@live.com>
2022-10-30 20:59:38 -1000

    added ping

--- src/app.js
+++ src/app.js
@@ -4,29 +4,35 @@ const crypto = require('crypto');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8080;
+const port = 443;
 const server = app.listen(port, () => {
-    //console.log(`listening at http://localhost:${port}`);
-    console.log(`listening at https://alerts-border-widget.onrender.com`);
+  //console.log(`listening at http://localhost:${port}`);
+  console.log(`listening at https://alerts-border-widget.onrender.com`);
 });
 const io = require('socket.io')(server, {
-    path: '/ws/'
+  path: '/ws/',
 });
 
+io.on('connection', (socket) => {
+  console.log('connected');
+
+  socket.on('pingy', () => {
+    console.log('pingy');
+    socket.emit('pongy');
+  });
+});
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-io.on('connection', (socket) => {
-	console.log('connected');
-})
-
 // Notification request headers
 const TWITCH_MESSAGE_ID = 'Twitch-Eventsub-Message-Id'.toLowerCase();
-const TWITCH_MESSAGE_TIMESTAMP = 'Twitch-Eventsub-Message-Timestamp'.toLowerCase();
-const TWITCH_MESSAGE_SIGNATURE = 'Twitch-Eventsub-Message-Signature'.toLowerCase();
+const TWITCH_MESSAGE_TIMESTAMP =
+  'Twitch-Eventsub-Message-Timestamp'.toLowerCase();
+const TWITCH_MESSAGE_SIGNATURE =
+  'Twitch-Eventsub-Message-Signature'.toLowerCase();
 const MESSAGE_TYPE = 'Twitch-Eventsub-Message-Type'.toLowerCase();
 
 // Notification message types
@@ -37,102 +43,109 @@ const MESSAGE_TYPE_REVOCATION = 'revocation';
 // Prepend this string to the HMAC that's created from the message
 const HMAC_PREFIX = 'sha256=';
 
-app.use(express.raw({          // Need raw message body for signature verification
-    type: 'application/json'
-}))
+app.use(
+  express.raw({
+    // Need raw message body for signature verification
+    type: 'application/json',
+  })
+);
 
 app.post('/eventsub', (req, res) => {
-    let secret = getSecret();
-    let message = getHmacMessage(req);
-    let hmac = HMAC_PREFIX + getHmac(secret, message);  // Signature to compare
+  console.log(req.body);
+  let secret = getSecret();
+  let message = getHmacMessage(req);
+  let hmac = HMAC_PREFIX + getHmac(secret, message); // Signature to compare
 
-    if (true === verifyMessage(hmac, req.headers[TWITCH_MESSAGE_SIGNATURE])) {
-        console.log("signatures match");
+  if (true === verifyMessage(hmac, req.headers[TWITCH_MESSAGE_SIGNATURE])) {
+    console.log('signatures match');
 
-        // Get JSON object from body, so you can process the message.
-        let notification = JSON.parse(req.body);
-        //console.log(notification);
-        
-        if (MESSAGE_TYPE_NOTIFICATION === req.headers[MESSAGE_TYPE]) {
-            
-            let type = notification.subscription.type
-            let user = notification.event.user_name;
-            switch (type) {
-                case 'channel.follow':
-                    console.log(`Received a follow from: ${ user }`);
-                    io.emit('alert', 'New Follow!', user);
-                    break;
-                case 'channel.subscribe':
-                    if (!notification.event.is_gift) {
-                        var tier = parseInt(notification.event.tier) / 1000;
-                        console.log(`Received a subscription from: ${ user }`);
-                        io.emit('alert', `New Tier ${ tier } Sub!`, user);
-                    }
-                    break;
-                case 'channel.subscription.gift':
-                    var tier = parseInt(notification.event.tier) / 1000;
-                    var total = notification.event.total;
-                    console.log(`User: ${ user } gifted ${ total } subs`);
-                    io.emit('alert', `${ total } gifted subs!`, user);
-                    break;
-                case 'channel.cheer':
-                    var bits = notification.event.bits;
-                    console.log(`User: ${ user } gifted ${ total } subs`);
-                    io.emit('alert', `Cheer: ${ bits }!`, user);
-                    break;
-                default:
-                    console.log(`Not handled: ${ type }`);
-                    io.emit('alert', `Alert! ${ type }`, user);
-                    break;
-            }
+    // Get JSON object from body, so you can process the message.
+    let notification = JSON.parse(req.body);
+    //console.log(notification);
 
-            console.log(`Event type: ${notification.subscription.type}`);
-            console.log(JSON.stringify(notification, null, 4));
-            
-            res.sendStatus(204);
-        }
-        else if (MESSAGE_TYPE_VERIFICATION === req.headers[MESSAGE_TYPE]) {
-            console.log(notification.challenge);
-            res.status(200).send(notification.challenge);
-        }
-        else if (MESSAGE_TYPE_REVOCATION === req.headers[MESSAGE_TYPE]) {
-            res.sendStatus(204);
+    if (MESSAGE_TYPE_NOTIFICATION === req.headers[MESSAGE_TYPE]) {
+      let type = notification.subscription.type;
+      let user = notification.event.user_name;
+      switch (type) {
+        case 'channel.follow':
+          console.log(`Received a follow from: ${user}`);
+          io.emit('alert', 'New Follow!', user);
+          break;
+        case 'channel.subscribe':
+          if (!notification.event.is_gift) {
+            var tier = parseInt(notification.event.tier) / 1000;
+            console.log(`Received a subscription from: ${user}`);
+            io.emit('alert', `New Tier ${tier} Sub!`, user);
+          }
+          break;
+        case 'channel.subscription.gift':
+          var tier = parseInt(notification.event.tier) / 1000;
+          var total = notification.event.total;
+          console.log(`User: ${user} gifted ${total} subs`);
+          io.emit('alert', `${total} gifted subs!`, user);
+          break;
+        case 'channel.cheer':
+          var bits = notification.event.bits;
+          console.log(`User: ${user} gifted ${total} subs`);
+          io.emit('alert', `Cheer: ${bits}!`, user);
+          break;
+        default:
+          console.log(`Not handled: ${type}`);
+          io.emit('alert', `Alert! ${type}`, user);
+          break;
+      }
 
-            console.log(`${notification.subscription.type} notifications revoked!`);
-            console.log(`reason: ${notification.subscription.status}`);
-            console.log(`condition: ${JSON.stringify(notification.subscription.condition, null, 4)}`);
-        }
-        else {
-            res.sendStatus(204);
-            console.log(`Unknown message type: ${req.headers[MESSAGE_TYPE]}`);
-        }
+      console.log(`Event type: ${notification.subscription.type}`);
+      console.log(JSON.stringify(notification, null, 4));
+
+      res.sendStatus(204);
+    } else if (MESSAGE_TYPE_VERIFICATION === req.headers[MESSAGE_TYPE]) {
+      console.log(notification.challenge);
+      res.status(200).send(notification.challenge);
+    } else if (MESSAGE_TYPE_REVOCATION === req.headers[MESSAGE_TYPE]) {
+      res.sendStatus(204);
+
+      console.log(`${notification.subscription.type} notifications revoked!`);
+      console.log(`reason: ${notification.subscription.status}`);
+      console.log(
+        `condition: ${JSON.stringify(
+          notification.subscription.condition,
+          null,
+          4
+        )}`
+      );
+    } else {
+      res.sendStatus(204);
+      console.log(`Unknown message type: ${req.headers[MESSAGE_TYPE]}`);
     }
-    else {
-        console.log('403');    // Signatures didn't match.
-        res.sendStatus(403);
-    }
-})
-
+  } else {
+    console.log('403'); // Signatures didn't match.
+    res.sendStatus(403);
+  }
+});
 
 function getSecret() {
-    return process.env.app_secret;
+  return process.env.app_secret;
 }
 
 // Build the message used to get the HMAC.
 function getHmacMessage(request) {
-    return (request.headers[TWITCH_MESSAGE_ID] + 
-        request.headers[TWITCH_MESSAGE_TIMESTAMP] + 
-        request.body);
+  return (
+    request.headers[TWITCH_MESSAGE_ID] +
+    request.headers[TWITCH_MESSAGE_TIMESTAMP] +
+    request.body
+  );
 }
 
 // Get the HMAC.
 function getHmac(secret, message) {
-    return crypto.createHmac('sha256', secret)
-    .update(message)
-    .digest('hex');
+  return crypto.createHmac('sha256', secret).update(message).digest('hex');
 }
 
 // Verify whether our hash matches the hash that Twitch passed in the header.
 function verifyMessage(hmac, verifySignature) {
-    return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(verifySignature));
+  return crypto.timingSafeEqual(
+    Buffer.from(hmac),
+    Buffer.from(verifySignature)
+  );
 }
--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,38 +1,41 @@
 const socket = io('wss://alerts-border-widget.onrender.com', {
-	path: '/ws/'
+  //const socket = io('ws://localhost:443', {
+  path: '/ws/',
 });
 
 const eventQueue = [];
 var waiting = true;
 
 socket.on('connect', () => {
-	console.log("Connected!");
-	eventPopup();
+  console.log('Connected!');
+  eventPopup();
 });
 
 socket.on('alert', (message, username) => {
-	console.log(message, username);
-	eventQueue.push({'msg': message, 'user': username});
-	console.log(eventQueue);
-	if (waiting) { eventPopup(); }
+  console.log(message, username);
+  eventQueue.push({ msg: message, user: username });
+  console.log(eventQueue);
+  if (waiting) {
+    eventPopup();
+  }
 });
 
 async function eventPopup() {
-	waiting = false;
-	if (eventQueue.length > 0) {
-		let next = eventQueue.shift();
-		let message = next.msg;
-		let username = next.user;
-		console.log(username);
-		console.log(message);
-		$('#alert-message').html(message + '<br>' + username);
-		$('.alert-container').addClass('slide-up');
-	}
-	await delay(3000);
-	$('.alert-container').removeClass('slide-up')
-	console.log(eventQueue);
-	await delay();	
-	(eventQueue.length > 0 == true) ? eventPopup() : waiting = true;
+  waiting = false;
+  if (eventQueue.length > 0) {
+    let next = eventQueue.shift();
+    let message = next.msg;
+    let username = next.user;
+    console.log(username);
+    console.log(message);
+    $('#alert-message').html(message + '<br>' + username);
+    $('.alert-container').addClass('slide-up');
+  }
+  await delay(3000);
+  $('.alert-container').removeClass('slide-up');
+  console.log(eventQueue);
+  await delay();
+  eventQueue.length > 0 == true ? eventPopup() : (waiting = true);
 }
 
 /**
@@ -41,7 +44,7 @@ async function eventPopup() {
  * @returns A Promise that resolves after waiting.
  */
 function delay(time = 1000) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve('resolved');
     }, time);
@@ -52,16 +55,33 @@ function delay(time = 1000) {
  * Used to clear the queue.
  */
 function emptyQueue() {
-	eventQueue.length = 0;
-	console.log("Cleared the queue!");
-	console.log(eventQueue);
+  eventQueue.length = 0;
+  console.log('Cleared the queue!');
+  console.log(eventQueue);
 }
 
 /**
  * Used to test the pop up animation
  */
 function testPopup() {
-	console.log("Testing popup!");
-	eventQueue.push({'msg': "This is a test", 'user': "testuser"});
-	if (waiting) { eventPopup(); }
+  console.log('Testing popup!');
+  eventQueue.push({ msg: 'This is a test', user: 'testuser' });
+  if (waiting) {
+    eventPopup();
+  }
 }
+
+var start;
+/**
+ * Pings the server.
+ */
+function ping() {
+  start = Date.now();
+  socket.emit('pingy');
+  console.log('Ping!');
+}
+
+socket.on('pongy', () => {
+  console.log('Time: ' + (Date.now() - start) + ' ms');
+  console.log('Pong!');
+});
