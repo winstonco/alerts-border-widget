ca938acf0c650a964e5a91eb74deb864956664ed
Winston Co <winstonco@live.com>
2022-10-31 02:20:49 -1000

    app updated

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ io.on('connection', (socket) => {
 });
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
