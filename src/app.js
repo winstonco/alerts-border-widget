568d56f27e315b762bcf72c2ad46d7e9a0576cf0
Winston Co <winstonco@live.com>
2022-11-13 11:53:28 -1000

    health path added

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/health', (req, res) => {
+  res.status(200);
+});
+
 app.use(express.json());
 
 app.use(cors());
